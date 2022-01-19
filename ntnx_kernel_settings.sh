fcee10fc18bf2f7644115ef26e258c6f9828f48d
Ross Davies <davies.ross@gmail.com>
2022-01-19 11:15:52 -0700

    Update ntnx_kernel_settings.sh
    Updated file format
    Added current settings backup

--- ntnx_kernel_settings.sh
+++ ntnx_kernel_settings.sh
@@ -1,28 +1,78 @@
 #!/usr/bin/env bash
-#
-# Set Nutanix Best Practices
-# Kernel Settings
-#
+##############################################################################
+## Configure kernel settings to Nutanix best practices
+##############################################################################
+## Files modified
+##
+##############################################################################
+## License
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+##############################################################################
+## References
+##
+## Linux on Nutanix AHV Best Practices guide
+## https://portal.nutanix.com/page/documents/solutions/details?targetId=BP-2105-Linux-on-AHV:BP-2105-Linux-on-AHV
+##
+##############################################################################
+## Notes
+##
+##############################################################################
 
+#timestamp
+/usr/bin/echo "** ntnx_kernel_settings.sh START" $(date +%F-%H%M-%S)
+
+#################
+## SET BASH ERREXIT OPTION
+#################
 set -o errexit
 
-echo "NTNX: Setting /proc/sys/vm/overcommit_memory = 1"
-echo 1 > /proc/sys/vm/overcommit_memory
+##################
+## SET VARIBLES
+##################
 
-echo "NTNX: Setting /proc/sys/vm/dirty_background_ratio = 5"
-echo 5 > /proc/sys/vm/dirty_background_ratio
+BACKUPDIR=/root/NtnxBestPractices
 
-echo "NTNX: Setting /proc/sys/vm/dirty_ratio = 15"
-echo 15 > /proc/sys/vm/dirty_ratio
+#################
+## BACKUP FILES
+#################
 
-echo "NTNX: Setting /proc/sys/vm/dirty_expire_centisecs = 500"
-echo 500 > /proc/sys/vm/dirty_expire_centisecs
+if [ ! -d "${BACKUPDIR}" ]; then mkdir -p ${BACKUPDIR}; fi
 
-echo "NTNX: Setting /proc/sys/vm/dirty_writeback_centisecs = 100"
-echo 100 > /proc/sys/vm/dirty_writeback_centisecs
+/bin/cp -fpd /proc/sys/vm/overcommit_memory ${BACKUPDIR}/sys_vm_overcommit_memory-DEFAULT
+/bin/cp -fpd /proc/sys/vm/dirty_background_ratio ${BACKUPDIR}/sys_vm_dirty_background_ratio-DEFAULT
+/bin/cp -fpd /proc/sys/vm/dirty_ratio ${BACKUPDIR}/sys_vm_dirty_ratio-DEFAULT
+/bin/cp -fpd /proc/sys/vm/dirty_expire_centisecs ${BACKUPDIR}/sys_vm_dirty_expire_centisecs-DEFAULT
+/bin/cp -fpd /proc/sys/vm/dirty_writeback_centisecs ${BACKUPDIR}/sys_vm_dirty_writeback_centisecs-DEFAULT
+/bin/cp -fpd /proc/sys/vm/swappiness ${BACKUPDIR}/sys_vm_swappiness-DEFAULT
 
-echo "NTNX: Setting /proc/sys/vm/swappiness = 0"
-echo 0 > /proc/sys/vm/swappiness
+#################
+## UPDATE CONFIGURATION
+#################
 
-# Save Settings
-sysctl -p
+/usr/bin/echo 1 > /proc/sys/vm/overcommit_memory
+/usr/bin/echo 5 > /proc/sys/vm/dirty_background_ratio
+/usr/bin/echo 15 > /proc/sys/vm/dirty_ratio
+/usr/bin/echo 500 > /proc/sys/vm/dirty_expire_centisecs
+/usr/bin/echo 100 > /proc/sys/vm/dirty_writeback_centisecs
+/usr/bin/echo 0 > /proc/sys/vm/swappiness
+
+#################
+## SAVE KERNEL SETTINGS
+#################
+/usr/sbin/sysctl -p
+
+#timestamp
+/usr/bin/echo "** ntnx_kernel_settings.sh COMPLETE" $(date +%F-%H%M-%S)
