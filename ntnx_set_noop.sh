d5e8e39390d0a180f8d1584619dd8d0da6d25af4
Ross Davies <davies.ross@gmail.com>
2022-01-19 11:29:24 -0700

    Update ntnx_set_noop.sh
    Removed old script content

--- ntnx_set_noop.sh
+++ ntnx_set_noop.sh
@@ -1,28 +1,6 @@
 #!/usr/bin/env bash
-#
-# Set Nutanix Best Practices
-# Set elevator to noop
-#
-
-set -o errexit
-
-# Update existing disks
-lsscsi | grep NUTANIX | awk '{print $NF}' | awk -F"/" '{print $NF}' | grep -v "-" | while read LUN
-do
-  echo "NTNX: Setting /sys/block/${LUN}/queue/scheduler = noop"
-  echo noop > /sys/block/${LUN}/queue/scheduler
-done
-
-# Set elevator=noop in /etc/default/grub
-echo "NTNX: Setting elevator=noop in /etc/default/grub"
-grep -q "elevator=" /etc/default/grub && sudo sed "s/^elevator=.*/elevator=noop/" -i /etc/default/grub || sudo sed -i 's/GRUB_CMDLINE_LINUX="[^"]*/& elevator=noop/' /etc/default/grub
-
-
-
-
-#!/usr/bin/env bash
 ##############################################################################
-## Disable Transparent Hugepages to meet Nutanix best practices
+## Set elevator to noop to meet Nutanix best practices
 ##############################################################################
 ## Files modified
 ##
