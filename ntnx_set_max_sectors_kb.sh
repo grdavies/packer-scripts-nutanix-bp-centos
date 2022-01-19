17bafc5d62f5f3d6f7458a3daa016c2666979dec
Ross Davies <davies.ross@gmail.com>
2022-01-19 11:16:24 -0700

    Update ntnx_set_max_sectors_kb.sh
    Updated file formatting

--- ntnx_set_max_sectors_kb.sh
+++ ntnx_set_max_sectors_kb.sh
@@ -1,20 +1,71 @@
 #!/usr/bin/env bash
-#
-# Set Nutanix Best Practices
-# Linux Disk Device Setting max_sectors_kb to 1024
-#
+##############################################################################
+## Configure each disk max_sectors_kb to Nutanix best practices
+##############################################################################
+## Files modified
+##
+##############################################################################
+## License
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+##############################################################################
+## References
+##
+## Linux on Nutanix AHV Best Practices guide
+## https://portal.nutanix.com/page/documents/solutions/details?targetId=BP-2105-Linux-on-AHV:BP-2105-Linux-on-AHV
+##
+##############################################################################
+## Notes
+##
+##############################################################################
 
+#timestamp
+/usr/bin/echo "** ntnx_set_max_sectors_kb.sh START" $(date +%F-%H%M-%S)
+
+#################
+## SET BASH ERREXIT OPTION
+#################
 set -o errexit
 
-# Update existing disks
+##################
+## SET VARIBLES
+##################
+
+BACKUPDIR=/root/NtnxBestPractices
+
+##################
+## CREATE UDEV RULE
+##################
+cat > ${BACKUPDIR}/99-nutanix-max_sectors_kb.rules << 'EOFUDEVRULE'
+ACTION=="add", SUBSYSTEMS=="scsi", ATTRS{vendor}=="NUTANIX ", ATTRS{model}=="VDISK", RUN+="/bin/sh -c 'echo 1024 >/sys$DEVPATH/queue/max_sectors_kb'"
+EOFUDEVRULE
+
+#####################
+## DEPLOY NEW FILES
+#####################
+/bin/cp -f ${BACKUPDIR}/99-nutanix-max_sectors_kb.rules /etc/udev/rules.d/99-nutanix-max_sectors_kb.rules
+/bin/chown root:root /etc/udev/rules.d/99-nutanix-max_sectors_kb.rules
+/bin/chmod 0600      /etc/udev/rules.d/99-nutanix-max_sectors_kb.rules
+
+##################
+## UPDATE ANY EXISTING DISKS
+##################
 lsscsi | grep NUTANIX | awk '{print $NF}' | awk -F"/" '{print $NF}' | grep -v "-" | while read LUN
 do
-  echo "NTNX: Setting /sys/block/${LUN}/queue/max_sectors_kb = 1024"
-  echo 1024 > /sys/block/${LUN}/queue/max_sectors_kb
+  /usr/bin/echo 1024 > /sys/block/${LUN}/queue/max_sectors_kb
 done
 
-# Add command to /etc/rc.local to update UDEV rule for any newly added disks
-echo "NTNX: Creating /etc/udev/rules.d/99-nutanix-max_sectors_kb.rules"
-tee -a /etc/udev/rules.d/99-nutanix-max_sectors_kb.rules > /dev/null <<EOT
-ACTION=="add", SUBSYSTEMS=="scsi", ATTRS{vendor}=="NUTANIX ", ATTRS{model}=="VDISK", RUN+="/bin/sh -c 'echo 1024 >/sys$DEVPATH/queue/max_sectors_kb'"
-EOT
+#timestamp
+/usr/bin/echo "** ntnx_set_max_sectors_kb.sh COMPLETE" $(date +%F-%H%M-%S)
